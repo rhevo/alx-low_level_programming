1ed5a93d2f34c0a3866204a893adc0a726f4bb2e
rhevo <eugenenarh1@gmail.com>
2023-10-26 10:50:45 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_binary -- prints binary representation of a number
+ * @n: long int
+ *
+ * Return: converted number, 0
+ */
+void print_binary(unsigned long int n)
+{
+
+}
